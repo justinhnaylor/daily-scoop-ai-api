00a35b68b7f24f7be86b1b38ff27171a88cc8248
Justin Naylor <justinnaylor@Justins-Mac-mini.local>
2025-02-18 07:57:32 -0700

    Updated daily and recent sh

--- go_daily.sh
+++ go_daily.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Set the timezone
 export TZ="America/New_York"
+
+# Add the Go binary location to the PATH (if it's not already in PATH)
+export PATH=$PATH:/usr/local/go/bin
+
+# Log the running time
 echo "Running at $(date)"
 
-
+# Navigate to the project directory
 cd /Users/justinnaylor/projects/daily-scoop-ai-api
+
+# Run the Go command
 go run . -mode=daily
--- go_recent.sh
+++ go_recent.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Set the timezone
 export TZ="America/New_York"
+
+# Add the Go binary location to the PATH (if it's not already in PATH)
+export PATH=$PATH:/usr/local/go/bin
+
+# Log the running times
 echo "Running at $(date)"
 
+# Navigate to the project directory
 cd /Users/justinnaylor/projects/daily-scoop-ai-api
+
+# Run the Go command
 go run . -mode=recent
